73f5f893b95a7bae02d575816360a16d17f3f70d
Nick DesLauriers <deslni01@gmail.com>
2022-03-29 10:42:02 -0500

    Weather done

---
+++ README.md
@@ -0,0 +1,30 @@
+# The Odin Project 2
+
+## Project: Weather App
+
+[Live Page Here]()
+
+### Comments:
+
+### Remaining Tasks:
+
+-   [x] Add search bar and JS to change location of weather
+-   [ ] Add NHL and MLB APIs for the sports sections
+
+### Assignment:
+
+-   [x] Set up a blank HTML document with the appropriate links to your JavaScript and CSS files.
+
+-   [x] Write the functions that hit the API. You’re going to want functions that can take a location and return the weather data for that location. For now, just console.log() the information.
+
+-   [x] Write the functions that process the JSON data you’re getting from the API and return an object with only the data you require for your app.
+
+-   [x] Set up a simple form that will let users input their location and will fetch the weather info (still just console.log() it).
+
+-   [x] Display the information on your webpage!
+
+-   [x] Add any styling you like!
+
+-   [ ] Optional: add a ‘loading’ component that displays from the time the form is submitted until the information comes back from the API.
+
+-   [ ] Push that baby to github and share your solution below!
--- script.js
+++ script.js
@@ -4,8 +4,6 @@ async function getWeatherByCity(city) {
 	);
 
 	const data = await response.json();
-	// console.log(data);
-	// console.log(data.name);
 	return data;
 }
 
@@ -41,7 +39,6 @@ async function processForecast(data) {
 	};
 }
 
-// this works both as async and without - does it need it?
 // note: this pulls as current local time, so for example 7am London will show as 12am MN on the page - should change to location time?
 function formatDateToString(date) {
 	const newDate = new Date(date * 1000);
@@ -91,13 +88,6 @@ async function currentWeatherDOM(city) {
 }
 
 async function forecastWeatherDOM(forecastData) {
-	// const data = await getWeatherByCity('Minneapolis');
-	// const currentData = await processCurrent(data);
-	// const lat = currentData.coord.lat;
-	// const lon = currentData.coord.lon;
-	// const forecast = await getWeatherByCoords(lat, lon);
-	// const forecastData = await processForecast(forecast);
-
 	for (let i = 1; i < 5; i++) {
 		const datePanel = document.querySelector(`.day-${i}`);
 		const date = datePanel.querySelector(`#date-${i}`);
@@ -123,96 +113,43 @@ async function forecastWeatherDOM(forecastData) {
 	}
 }
 
-async function app() {
-	const data = await getWeatherByCity('London');
+async function app(cityName) {
+	const data = await getWeatherByCity(cityName);
 	const currentData = await processCurrent(data);
 	const lat = currentData.coord.lat;
 	const lon = currentData.coord.lon;
 
-	// // console.log(lat);
-	// // console.log(lon);
-
 	const forecast = await getWeatherByCoords(lat, lon);
-	// // console.log(forecast);
+
 	const forecastData = await processForecast(forecast);
 
 	const currentWeather = await currentWeatherDOM(currentData.name);
-	// const forecastWeather = await forecastWeatherDOM(currentWeather);
+
 	const forecastWeather = await forecastWeatherDOM(forecastData);
 }
 
-app();
+app('Minneapolis');
 
-// should make an outline of what needs to be done - each step, then go from there. not breaking this up right now, just trying isn't working
+const searchCity = document.querySelector('.search-city');
+const searchBar = document.querySelector('#city-search');
+const searchBtn = document.querySelector('#search-btn');
 
-// 'one call api' must be called via latitude and longitude - so I think I need to fetch by city name, then return lat/lon, then do a call with ${lat} and ${lon} to get the correct forecast
+searchCity.addEventListener('submit', (e) => {
+	e.preventDefault();
+	app(searchBar.value);
+	searchBar.value = '';
+});
 
-// daily forecast API call to get lat/longitude to for 'one call api'
-
-// return lat/lon, other current info
+// 'one call api' must be called via latitude and longitude - so I need to fetch by city name, then return lat/lon, then do a call with ${lat} and ${lon} to get the correct forecast
 
 // API call for 'one call api' - lat/lon as parameters
 // https://api.openweathermap.org/data/2.5/onecall?lat=33.44&lon=-94.04&exclude=hourly,minutely,current&appid=463eb7e5d0258362c22d75c2d2b53e4f&units=imperial
 
 // this excludes hourly, minutely, and current
 
-// start by getting things pulled, then console.log each day
-
-// then can do dom manipulation
-
 // search bar
 
-/*
-The timestamp/date comes in unix code: 1648063791
-let unix_timestamp = 1648063791
-let date = new Date(unix_timestamp * 1000)
-will return "Wed Mar 23 2022 blahblahblah"
-
-To get the day working:
-date.getDate() // returns 23 - will this work with single digits?
-
-date.getMonth() // returns 2 - need to get it to say 'Mar'
-date.getMonth() + 1 to easily make it accurate, lmao
-
-Can to make a function to format:
-function formatDateToString(date) {
-    var dd = (date.getDate() < 10 ? '0' : '') + date.getDate();
-    var MM = ((date.getMonth() + 1) < 10 ? '0' : '') + (date.getMonth() + 1);
-}
-*/
-
 // also maybe throw in wild/twins stuff
-
-/*
-Weather things:
-- current api
-    - name of city
-    - lat/lon of city
-    - 'main' - description
-    - icon
-    - main: temp
-    - main: feels like
-    - main: temp-min
-    - main: temp-max
-    - wind: speed
-    - clouds: all %
-    - sys: sunrise
-    - sys: sunset
-
-- forecast api
-    - iterate over daily[i]
-    - dt - date
-    - sunrise
-    - sunset
-    - temp: day
-    - temp: min
-    - temp: max
-    - feels_like: day
-    - wind_speed
-    - weather[i]: main/description
-    - weather[i]: icon
-    - weather: clouds
-*/
 
 // https://statsapi.web.nhl.com/api/v1/standings?expand=standings.record
 
